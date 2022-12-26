4a6b33f74e003112b6a6f019aef57e0fc7f52cb6
Curt-Lucas <60093618+Curt-Lucas@users.noreply.github.com>
2022-12-25 19:16:03 -0500

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -11,29 +11,20 @@ with open('wallets1.txt', 'r') as file:
 outputfile = 'output.txt'
 
 def bruteforce(y):
-    
     print(f'Instance y{y + 1}: - Starting...')
-    
     pkey = PrivateKey()
-    
     for passphrase in wallets:
-        
         pkey.from_passphrase(passphrase)
         wif1 = pkey.privatekey_to_wif(compressed=True)
         wif2 = pkey.privatekey_to_wif()
         key1 = Key(wif1)
         key2 = Key(wif2)
-        
         if (key1.address or key2.address) in wallets:
-            print(f'y{y + 1} Got a Match! C: {key1.address} : P: {passphrase}')
-            print(f'y{y + 1} Got a Match! U: {key2.address} : P: {passphrase}')
             with open(outputfile, 'a') as result:
                 result.write(f'P: {passphrase} - C: {key1.address} - U: {key2.address}')
 
 def debug_bruteforce(y):
-    
     print(f'Instance y{y + 1}: - Starting...')
-    
     pkey = PrivateKey()
     for passphrase in wallets:
         pkey.from_passphrase(passphrase)
@@ -41,17 +32,13 @@ def debug_bruteforce(y):
         wif2 = pkey.privatekey_to_wif()
         key1 = Key(wif1)
         key2 = Key(wif2)
-
-        print(f'Instance y{y + 1}: - {key1.address} : {key2.address} : {passphrase} : 0')
-        
+        print(f'Instance y{y + 1}: C: {key1.address} U: {key2.address} P: {passphrase} : 0')
         if (key1.address or key2.address) in wallets:
-            print(f'y{y + 1} Got a Match! C: {key1.address} : P: {passphrase}')
-            print(f'y{y + 1} Got a Match! U: {key2.address} : P: {passphrase}')
+            print(f'y{y + 1} Got a Match! U: {key2.address} C: {key1.address} P: {passphrase}')
             with open(outputfile, 'a') as result:
                 result.write(f'P: {passphrase} - C: {key1.address} - U: {key2.address}')
                 
 def main():
-  
     menu_string = 'We\'re all mad here.\m\m\m\mA tool by Bigcointalk.org\n\n\n\nSelect bruteforce mode:\n'
     mode = (None, bruteforce, debug_bruteforce)
     for count, function in enumerate(mode):
@@ -63,22 +50,18 @@ def main():
         except AttributeError:
             menu_string += f'{count} - Exit\n'
     print(menu_string)
-
     try:
         choice = int(input('> '))
-        print(f'On a scale from 1 to {cpu_count()}, how much hell would you like to raise?')
+        print(f'How many instances would you like? (max={cpu_count()})')
         cpu_cores = int(input('> '))
         cpu_cores = cpu_cores if 0 < cpu_cores < cpu_count() else cpu_count()
         option = choice if 0 < choice <= len(mode) - 1 else 0
     except ValueError:
         option = 0
         cpu_cores = 0
-
-    print(f'Starting bruteforce with {cpu_cores} hardcore(s)...\n')
-    
+    print(f'Starting bruteforce with {cpu_cores} instances...\n')
     if mode[option] and 'debug' in mode[option].__name__:
         print(f'Debug mode...\n')
-        
     instances = []
     for i in range(cpu_cores):
         instance = Process(target=mode[option], args=(i))
@@ -86,8 +69,6 @@ def main():
         instance.start()
     for instance in instances:
         instance.join()
-
     print('tehehe')
-
 if __name__ == '__main__':
     main()
